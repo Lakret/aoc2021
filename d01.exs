8f37f5bfe4f516a877421a0fc0f9fafb68993f50
lakret <Rakuretu@gmail.com>
2021-12-01 22:48:44 +0100

    d01 p2

--- d01.exs
+++ d01.exs
@@ -1,7 +1,9 @@
 defmodule D01 do
-  def p1(report) do
+  def p1(report, elems \\ 1) do
     {_, increases} =
-      Enum.reduce(report, {nil, 0}, fn
+      Enum.chunk_every(report, elems, 1, :discard)
+      |> Enum.map(&Enum.sum/1)
+      |> Enum.reduce({nil, 0}, fn
         value, {nil, increases} -> {value, increases}
         value, {prev_value, increases} when prev_value < value -> {value, increases + 1}
         value, {_prev_value, increases} -> {value, increases}
@@ -36,6 +38,7 @@ input =
     v
   end)
 
-IO.inspect(input)
+assert D01.p1(input) |> IO.inspect(label: :p1) == 1298
 
-assert D01.p1(input) == 1298
+assert D01.p1(test_report, 3) == 5
+assert D01.p1(input, 3) |> IO.inspect(label: :p2) == 1248
---
+++ d01.sh
@@ -0,0 +1,1 @@
+elixir d01.exs
