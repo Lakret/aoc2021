d45e6a3021de7d447f5ca48ad402eb07cc899a0b
lakret <Rakuretu@gmail.com>
2021-12-12 15:17:38 +0100

    d12 p2

--- d12/src/main.rs
+++ d12/src/main.rs
@@ -67,6 +67,77 @@ fn dfs(g: &G) -> Vec<Path> {
     paths
 }
 
+fn dfs2(g: &G) -> Vec<Path> {
+    let mut stack = vec!["start".to_string()];
+    let mut paths = vec![];
+
+    let mut vertex_to_paths = HashMap::new();
+    vertex_to_paths.insert("start".to_string(), vec![vec!["start".to_string()]]);
+
+    while let Some(v) = stack.pop() {
+        if let Some(adjacent) = g.get(&v) {
+            if let Some(paths_ending_with_v) = vertex_to_paths.remove(&v) {
+                for w in adjacent.iter() {
+                    if w == "end" {
+                        for mut path in paths_ending_with_v.iter().cloned() {
+                            path.push(w.to_string());
+                            paths.push(path);
+                        }
+                    } else if w != "start" {
+                        let mut found_new_paths = false;
+                        let paths_ending_with_w = vertex_to_paths.entry(w.to_string()).or_default();
+
+                        for mut partial_path in paths_ending_with_v.iter().cloned() {
+                            if can_be_extended_with(&partial_path, &w) {
+                                partial_path.push(w.clone());
+
+                                paths_ending_with_w.push(partial_path);
+                                found_new_paths = true;
+                            }
+                        }
+
+                        if found_new_paths {
+                            stack.push(w.clone());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    paths
+}
+
+fn can_be_extended_with(path: &Path, vertex: &str) -> bool {
+    if is_visit_once_vertex(vertex) {
+        let mut counts: HashMap<String, usize> = HashMap::new();
+        *counts.entry(vertex.to_string()).or_default() += 1;
+
+        for another_vertex in path.iter() {
+            if is_visit_once_vertex(another_vertex) {
+                *counts.entry(another_vertex.clone()).or_default() += 1;
+            }
+        }
+
+        let mut double_appearances = 0;
+        for value in counts.values() {
+            if *value == 2 {
+                double_appearances += 1;
+            }
+
+            if *value > 2 {
+                return false;
+            }
+        }
+
+        if double_appearances > 1 {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 fn is_visit_once_vertex(v: &str) -> bool {
     v.chars().next().unwrap().is_lowercase()
 }
@@ -84,5 +155,8 @@ mod tests {
         let input = fs::read_to_string("../d12_input").unwrap();
         let g = parse(&input);
         assert_eq!(dfs(&g).len(), 4970);
+
+        assert_eq!(dfs2(&test_g).len(), 3509);
+        assert_eq!(dfs2(&g).len(), 4970);
     }
 }
