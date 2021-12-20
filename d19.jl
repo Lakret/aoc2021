fc3153190be3adadc690ffea34748845e9cd0edb
lakret <Rakuretu@gmail.com>
2021-12-20 14:59:54 +0100

    d19: fine tuning translations

--- d19.jl
+++ d19.jl
@@ -62,7 +62,7 @@ as a matrix of `[offset rdim sign]`.
 `same` should be a dictionary of matching coordinates with left scanner's perspective
 as keys, and right scanner's perspective as values.
 """
-function find_scanner_translation_matrix(same::Dict{Vector{Int32},Vector{Int32}})::Matrix{Int32}
+function find_translation_matrix(same::Dict{Vector{Int32},Vector{Int32}})::Matrix{Int32}
     l, r = [], []
     for entry in same
         push!(l, entry[1])
@@ -90,7 +90,7 @@ end
 Translates coordinates from right scanner's view `coords` to origin scanner's view,
 using the translation matrix `to_scanner` (returned from `find_scanner_translation_matrix`).
 """
-function translate_coords(coords::Vector{Int}, right_translation; left_translation = [0 1 1; 0 2 1; 0 3 1])
+function translate_coords(coords, right_translation; left_translation = [0 1 1; 0 2 1; 0 3 1])
     new_coords = [0, 0, 0]
 
     for dim = 1:3
@@ -101,17 +101,132 @@ function translate_coords(coords::Vector{Int}, right_translation; left_translati
     new_coords
 end
 
-same01 = find_matching(test_reports[0], test_reports[1])
-same41 = find_matching(test_reports[1], test_reports[4])
+function translate_translation_matrix(m::Matrix{Int32}, right_translation)::Matrix{Int32}
+    m2 = copy(m)
+    m2[:, 1] = translate_coords(m[:, 1], right_translation)
+    m2[:, 3] = m2[:, 3] .* right_translation[:, 3]
+    m2
+end
 
-ori1 = find_matching_scanner_coords(same01)
-ori4_from1 = find_matching_scanner_coords(same41)
-ori4_from0 = translate_coords(ori4[:, 1], ori1)
+same01 = find_matching(test_reports[0], test_reports[1])
+same14 = find_matching(test_reports[1], test_reports[4])
+same42 = find_matching(test_reports[4], test_reports[2])
+same13 = find_matching(test_reports[1], test_reports[3])
+
+ori1 = find_translation_matrix(same01)
+ori4_from1 = find_translation_matrix(same14)
+ori4 = translate_translation_matrix(ori4_from1, ori1)
+ori2_from4 = @pipe find_translation_matrix(same42) |> translate_translation_matrix(_, ori4)
+ori2 = @pipe find_translation_matrix(same42) |> translate_translation_matrix(_, ori4)
+ori3 = @pipe find_translation_matrix(same13) |> translate_translation_matrix(_, ori1)
 
 fixed_keys = collect(keys(same))
 map(coords -> translate_coords(coords, ori1), [same[k] for k in fixed_keys]) == fixed_keys
 map(coords -> translate_coords(coords, ori2), [same[k] for k in fixed_keys]) == fixed_keys
+map(coords -> translate_coords(coords, ori4_from0), values(same41))
 
+all_coords = Set() # should be 79, but we get more and not all test ones are here
+for c in eachrow(test_reports[0])
+    push!(all_coords, c)
+end
+for c in eachrow(test_reports[1])
+    push!(all_coords, translate_coords(c, ori1))
+end
+for c in eachrow(test_reports[2])
+    push!(all_coords, translate_coords(c, ori2))
+end
+for c in eachrow(test_reports[3])
+    push!(all_coords, translate_coords(c, ori3))
+end
+for c in eachrow(test_reports[4])
+    push!(all_coords, translate_coords(c, ori4))
+end
+
+expected_all = [
+    [-892, 524, 684],
+    [-876, 649, 763],
+    [-838, 591, 734],
+    [-789, 900, -551],
+    [-739, -1745, 668],
+    [-706, -3180, -659],
+    [-697, -3072, -689],
+    [-689, 845, -530],
+    [-687, -1600, 576],
+    [-661, -816, -575],
+    [-654, -3158, -753],
+    [-635, -1737, 486],
+    [-631, -672, 1502],
+    [-624, -1620, 1868],
+    [-620, -3212, 371],
+    [-618, -824, -621],
+    [-612, -1695, 1788],
+    [-601, -1648, -643],
+    [-584, 868, -557],
+    [-537, -823, -458],
+    [-532, -1715, 1894],
+    [-518, -1681, -600],
+    [-499, -1607, -770],
+    [-485, -357, 347],
+    [-470, -3283, 303],
+    [-456, -621, 1527],
+    [-447, -329, 318],
+    [-430, -3130, 366],
+    [-413, -627, 1469],
+    [-345, -311, 381],
+    [-36, -1284, 1171],
+    [-27, -1108, -65],
+    [7, -33, -71],
+    [12, -2351, -103],
+    [26, -1119, 1091],
+    [346, -2985, 342],
+    [366, -3059, 397],
+    [377, -2827, 367],
+    [390, -675, -793],
+    [396, -1931, -563],
+    [404, -588, -901],
+    [408, -1815, 803],
+    [423, -701, 434],
+    [432, -2009, 850],
+    [443, 580, 662],
+    [455, 729, 728],
+    [456, -540, 1869],
+    [459, -707, 401],
+    [465, -695, 1988],
+    [474, 580, 667],
+    [496, -1584, 1900],
+    [497, -1838, -617],
+    [527, -524, 1933],
+    [528, -643, 409],
+    [534, -1912, 768],
+    [544, -627, -890],
+    [553, 345, -567],
+    [564, 392, -477],
+    [568, -2007, -577],
+    [605, -1665, 1952],
+    [612, -1593, 1893],
+    [630, 319, -379],
+    [686, -3108, -505],
+    [776, -3184, -501],
+    [846, -3110, -434],
+    [1135, -1161, 1235],
+    [1243, -1093, 1063],
+    [1660, -552, 429],
+    [1693, -557, 386],
+    [1735, -437, 1738],
+    [1749, -1800, 1813],
+    [1772, -405, 1572],
+    [1776, -675, 371],
+    [1779, -442, 1789],
+    [1780, -1548, 337],
+    [1786, -1538, 337],
+    [1847, -1591, 415],
+    [1889, -1729, 1762],
+    [1994, -1805, 1792]
+]
+
+
+map(coords -> translate_coords(coords, ori1), values(same01))
+map(coords -> translate_coords(coords, ori1), values(same14))
 
 
 for x in keys(test_reports), y in keys(test_reports)
