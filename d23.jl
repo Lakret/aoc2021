a4f5e409b5df5123c7b5c79384e07c0088ba41d2
Lakret <rakuretu@gmail.com>
2021-12-24 03:05:09 +0100

    moves generator

--- d23.jl
+++ d23.jl
@@ -11,6 +11,9 @@ using DataStructures
 
 target = [:A :A; :B :B; :C :C; :D :D]
 
+atype_to_step_cost = Dict(:A => 1, :B => 10, :C => 100, :D => 1000)
+atype_to_room_id = Dict(:A => 1, :B => 2, :C => 3, :D => 4)
+
 # #12.3.4.5.67# - corridor_cell_id
 # ###1#2#3#4### - room_id
 
@@ -34,7 +37,7 @@ function do_move(state, move)
         steps = get_out_steps + steps_to_goal_in_corridor[room_id, corridor_cell_id]
 
         atype = state["rooms"][room_id, room_pos_id]
-        cost = steps * atype_to_step_cost(atype)
+        cost = steps * atype_to_step_cost[atype]
 
         new_state = deepcopy(state)
         new_state["rooms"][room_id, room_pos_id] = missing
@@ -48,7 +51,7 @@ function do_move(state, move)
         steps = get_in_steps + steps_to_goal_in_corridor[room_id, corridor_cell_id]
 
         atype = state["corridor"][corridor_cell_id]
-        cost = steps * atype_to_step_cost(atype)
+        cost = steps * atype_to_step_cost[atype]
 
         new_state = deepcopy(state)
         new_state["rooms"][room_id, room_pos_id] = atype
@@ -71,17 +74,56 @@ function run(state, moves)
     (total_cost, state)
 end
 
-function atype_to_step_cost(atype)
-    if atype == :A
-        1
-    elseif atype == :B
-        10
-    elseif atype == :C
-        100
-    elseif atype == :D
-        1000
+
+function possible_moves(state)
+    moves = []
+
+    corridor_free_ids = findall(ismissing, state["corridor"])
+    # idx is room_id, tuple is (corridor_cell_id before room, corridor_cell_id after rooms)
+    room_id_to_prev_and_next_corridor_cell_id = [(2, 3), (3, 4), (4, 5), (5, 6)]
+
+    # we can either move out of a room...
+    for room_id = 1:4
+        # ... if there is somebody inside
+        if any((!) ∘ ismissing, state["rooms"][room_id, :])
+            (prev_cell_id, next_cell_id) = room_id_to_prev_and_next_corridor_cell_id[room_id]
+
+            # ... to any free spot in the corridor;
+            for corridor_cell_id in corridor_free_ids
+                cells_to_walk =
+                    if corridor_cell_id <= prev_cell_id
+                        state["corridor"][corridor_cell_id:prev_cell_id]
+                    else
+                        state["corridor"][next_cell_id:corridor_cell_id]
+                    end
+
+                if isempty(skipmissing(cells_to_walk))
+                    push!(moves, (:to_corridor, room_id, corridor_cell_id))
+                end
+            end
+        end
     end
+
+    # or, we can move into a room, if it's our final room,
+    # and if we don't cross anybody
+    corridor_occupied_ids = findall((!) ∘ ismissing, state["corridor"])
+    for corridor_cell_id in corridor_occupied_ids
+        atype = state["corridor"][corridor_cell_id]
+        room_id = atype_to_room_id[atype]
+
+        (prev_cell_id, next_cell_id) = room_id_to_prev_and_next_corridor_cell_id[room_id]
+
+        if (corridor_cell_id ∈ [prev_cell_id, next_cell_id]) ||
+           (corridor_cell_id < prev_cell_id &&
+            isempty(skipmissing(state["corridor"][corridor_cell_id+1:prev_cell_id]))) ||
+           (isempty(skipmissing(state["corridor"][next_cell_id:corridor_cell_id-1])))
+            push!(moves, (:to_room, room_id, corridor_cell_id))
+        end
+    end
+
+    moves
 end
+
 
 
 # rooms matrix:
